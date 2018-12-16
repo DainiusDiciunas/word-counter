d668b2d23f2179d85df31ea685c9d1ad3f7b8975
Dainius Dičiūnas <dainius.diciunas@programmatus.com>
2018-12-16 16:04:12 +0200

    Initialized project.

---
+++ src/main/java/com/dainiusd/words_counter/WordsCounterApplication.java
@@ -0,0 +1,14 @@
+package com.dainiusd.words_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordsCounterApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/dainiusd/words_counter/WordsCounterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dainiusd.words_counter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordsCounterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
